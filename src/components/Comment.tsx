d4deb48f2924a8ede7ac4e8e49eccf42c52522c7
Samuel Ramos <104317445+Thesamramos@users.noreply.github.com>
2023-11-22 14:15:50 -0400

    Ignite Feed

---
+++ src/components/Comment.tsx
@@ -0,0 +1,52 @@
+import { Avatar } from './Avatar';
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
+
+import styles from './Comment.module.css';
+
+interface CommentProps {
+    content: string,
+    onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount(likeCount + 1);
+    }
+
+    return(
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/Thesamramos.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Samuel Batista</strong>
+                            <time title='05 de novembro às 15:08h' dateTime='2023-11-05 15:08'>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
+                            <Trash size={24}/>
+                        </button>
+                    </header>
+
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>  
+    )
+}
